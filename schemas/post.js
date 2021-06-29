0421c48025998b2a269a2478babfe03b5068cc1c
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-06-30 02:27:34 +0600

    uploading xponent-server-side

---
+++ schemas/post.js
@@ -0,0 +1,63 @@
+export default {
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'location',
+      title: 'Location',
+      type: 'geopoint',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'mainImage',
+      title: 'Main Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    },
+    {
+      name: 'id',
+      title: 'Id',
+      type: 'number',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'blockContent',
+    },
+  ],
+
+  // preview: {
+  //   select: {
+  //     title: 'title',
+  //     author: 'author.name',
+  //     media: 'mainImage',
+  //   },
+  //   prepare(selection) {
+  //     const {author} = selection
+  //     return Object.assign({}, selection, {
+  //       subtitle: author && `by ${author}`,
+  //     })
+  //   },
+  // },
+}
